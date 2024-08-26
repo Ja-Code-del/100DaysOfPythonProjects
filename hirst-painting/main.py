cce690a4421227c0e37395d2f6815aa3bb77a16c
Bret Walda <82909109+Ja-Code-del@users.noreply.github.com>
2024-08-26 00:52:10 +0000

    Created a new function and added to utils.py : a function to automate the creation of paintings. :) Easier now.

--- hirst-painting/main.py
+++ hirst-painting/main.py
@@ -1,50 +1,27 @@
 import turtle as t
 import random
+from utils import hirst_dots_drawer
 
 tom = t.Turtle()
 t.colormode(255)
 canva = t.Screen()
-canva.setup(640, 380)
+canva.setup(680, 680)
 
+# make tom starts at the lowest of the screen
 tom.penup()
-tom.backward(150)
+tom.backward(320)
 tom.right(90)
-tom.forward(160)
+tom.forward(300)
 tom.left(90)
 
-x = 32
 tom.speed("fastest")
 
+colors = [(218, 173, 125), (159, 181, 190), (134, 73, 53), (50, 103, 154), (118, 81, 92), (179, 142, 152),
+          (162, 104, 151), (42, 47, 66), (128, 174, 115), (83, 96, 183), (67, 9, 27), (82, 133, 107), (52, 63, 78),
+          (228, 189, 141), (194, 91, 72), (220, 226, 221), (62, 49, 38), (115, 41, 56), (91, 143, 118), (70, 67, 52),
+          (209, 181, 189), (181, 185, 210), (209, 183, 178), (89, 55, 47), (183, 201, 179), (172, 199, 204),
+          (41, 73, 83)]
 
-colors = [(218, 173, 125), (159, 181, 190), (134, 73, 53), (50, 103, 154), (118, 81, 92), (179, 142, 152), (162, 104, 151), (42, 47, 66), (128, 174, 115), (83, 96, 183), (67, 9, 27), (82, 133, 107), (52, 63, 78), (228, 189, 141), (194, 91, 72), (220, 226, 221), (62, 49, 38), (115, 41, 56), (91, 143, 118), (70, 67, 52), (209, 181, 189), (181, 185, 210), (209, 183, 178), (89, 55, 47), (183, 201, 179), (172, 199, 204), (41, 73, 83)]
-
-# METHODE WITHOUT DOT()
-# for _ in range(50):
-#     for _ in range(int(600 / (x + 2))):
-#         tom.pensize(x)
-#         tom.pendown()
-#         tom.color(random.choice(colors))
-#         tom.forward(1)
-#         tom.penup()
-#         tom.forward(x + 1)
-#     tom.penup()
-#     tom.left(90)
-#     tom.forward(10)
-#     tom.right(90)
-#     tom.backward(600)
-
-tom.pensize(x)
-for _ in range(10):
-    for _ in range(10):
-        tom.pendown()
-        tom.dot(32, random.choice(colors))
-        tom.penup()
-        tom.forward(x + 1)
-    print(tom.pos())
-    tom.penup()
-    tom.left(90)
-    tom.forward(x + 1)
-    tom.right(90)
-    tom.backward(330)
+hirst_dots_drawer(tom, colors, 1, 300, 300, 5)
 
 t.exitonclick()
--- hirst-painting/utils.py
+++ hirst-painting/utils.py
@@ -1,5 +1,5 @@
 import colorgram as c
-
+import random
 
 def rgb_colors_extractor(image, numb_of_color):
     """extract rgb mode colors from an image.
@@ -17,3 +17,20 @@ def rgb_colors_extractor(image, numb_of_color):
         g = color.g
         b = color.b
         rgb_colors.append((r, g, b))
+
+
+def hirst_dots_drawer(tom, colors, space_between_dots, numb_dot_per_line, numb_of_lines, dots_size):
+    """Function to draw dots like Hirst painting : tom is your turtle name.
+    colors is a list of rgb colors"""
+    for _ in range(numb_of_lines):
+        for _ in range(numb_dot_per_line):
+            tom.pendown()
+            tom.dot(dots_size, random.choice(colors))
+            tom.penup()
+            tom.forward(dots_size + space_between_dots)
+        # print(tom.pos())
+        tom.penup()
+        tom.left(90)
+        tom.forward(dots_size + space_between_dots)
+        tom.right(90)
+        tom.backward((dots_size * numb_dot_per_line) + (space_between_dots * numb_dot_per_line))
