6b498267980cbebc1bfbef03c850dbaa8417bbed
Bret Walda <82909109+Ja-Code-del@users.noreply.github.com>
2024-08-28 16:36:07 +0000

    my version of the challenge

---
+++ day-19-start/race.py
@@ -0,0 +1,67 @@
+from turtle import Turtle, Screen
+import random as r
+
+tim = Turtle()
+don = Turtle()
+raph = Turtle()
+mike = Turtle()
+screen = Screen()
+screen.setup(640, 480)
+
+
+# user_bet = screen.textinput("Make Your Bet", "Who will win? : ")
+
+
+# state of Turtles function
+def set_turtle_state(turtle_name, color, y_pos):
+    turtle_name.shape("turtle")
+    turtle_name.color(color)
+    turtle_name.penup()
+    turtle_name.goto(-310, y_pos)
+
+
+def check_arrive(turtle_name):
+    if turtle_name.xcor() > 300:
+        return True
+
+
+# state of Tim
+set_turtle_state(tim, "green", 80)
+
+# state of Don
+set_turtle_state(don, "blue", 160)
+
+# state of Raph
+set_turtle_state(raph, "purple", -80)
+
+# state of Mike
+set_turtle_state(mike, "yellow", -160)
+racers = {"don": don,
+          "tim": tim,
+          "raph": raph,
+          "mike": mike}
+
+checker = True
+for key, value in racers.items():
+    while value.xcor() < 300 and checker:
+        for key1, value1 in racers.items():
+            speed = r.randint(0, 10)
+            value1.forward(speed)
+            if check_arrive(value1):
+                print(f"{key}'s arrived")
+                checker = False
+        # tim.forward(speed)
+        # if check_arrive(tim):
+        #     print("Tim's arrived")
+        # speed = r.randint(0, 10)
+        # don.forward(speed)
+        # check_arrive(don)
+        # speed = r.randint(0, 10)
+        # raph.forward(speed)
+        # check_arrive(raph)
+        # speed = r.randint(0, 10)
+        # mike.forward(speed)
+        # check_arrive(mike)
+        # print(tim.pos())
+
+screen.exitonclick()
