1b96f639de15b662feaef8fdb84c9e3d8ae8a5b1
Bret Walda <82909109+Ja-Code-del@users.noreply.github.com>
2024-08-29 22:52:53 +0000

    update
    Now the snake moves according to keybord

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,23 +1,22 @@
 from turtle import Screen
 import time
 from snake import Snake
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 #CREATE SNAKE BODY
-snake = Snake("gray")
+snake = Snake()
+screen.listen()
 
 #MOVE THE SNAKE
 #create the variable game_is_on
-screen.update()
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    # how to make the square follow the head?
-    # The last square should take the position of the following and so-and-so until the first : the head
     snake.move()
 
 
--- snake_game/snake.py
+++ snake_game/snake.py
@@ -1,22 +1,46 @@
-from turtle import Turtle
+from turtle import Turtle, Screen
+
+MOVE_DISTANCE = 20
+screen = Screen()
 
 
 class Snake:
-    def __init__(self, snake_color):
+    def __init__(self):
         self.turtles = []
+        self.create_snake()
+
+    def create_snake(self):
         for _ in range(3):
             new_turtle = Turtle("square")
-            new_turtle.color(snake_color)
+            new_turtle.color("gray")
             self.turtles.append(new_turtle)
         for turtle in self.turtles:
             turtle.penup()
             turtle.goto(self.turtles.index(turtle) * (-20), 0)
 
     def move(self):
+        """Move the snake"""
+        # how to make the square follow the head?
+        # The last square should take the position of the following and so-and-so until the first : the head
         for square_index in range(len(self.turtles) - 1, 0, -1):
             new_x = self.turtles[square_index - 1].xcor()
             new_y = self.turtles[square_index - 1].ycor()
             self.turtles[square_index].goto(new_x, new_y)
 
-        self.turtles[0].forward(20)
+        screen.onkeypress(self.go_up, "Up")
+        screen.onkeypress(self.go_down, "Down")
+        screen.onkeypress(self.go_left, "Left")
+        screen.onkeypress(self.go_right, "Right")
+        self.turtles[0].forward(MOVE_DISTANCE)
+
+    def go_up(self):
         self.turtles[0].setheading(90)
+
+    def go_down(self):
+        self.turtles[0].setheading(270)
+
+    def go_left(self):
+        self.turtles[0].setheading(180)
+
+    def go_right(self):
+        self.turtles[0].setheading(0)
