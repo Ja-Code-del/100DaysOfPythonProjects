dc48b83415bd0b1470499130d4dd4d76328d7423
Bret Walda <82909109+Ja-Code-del@users.noreply.github.com>
2024-08-29 18:59:49 +0000

    Before I make OOP version

--- snake_game/main.py
+++ snake_game/main.py
@@ -28,8 +28,16 @@ screen.update()
 game_is_on = True
 while game_is_on:
     screen.update()
-    for square in turtles:
-        square.forward(20)
-        time.sleep(1)
+    time.sleep(0.1)
+    # how to make the square follow the head?
+    # The last square should take the position of the following and so-and-so until the first : the head
+    for square_index in range(len(turtles) - 1, 0, -1):
+        new_x = turtles[square_index - 1].xcor()
+        new_y = turtles[square_index - 1].ycor()
+        turtles[square_index].goto(new_x, new_y)
+
+    turtles[0].forward(20)
+    turtles[0].setheading(90)
+
 
 screen.exitonclick()
