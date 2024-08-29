20c1ae2829dece9944e8f9986f7651c7564686a7
Bret Walda <82909109+Ja-Code-del@users.noreply.github.com>
2024-08-29 07:29:18 +0000

    Update main
    called the method tracer to stop the refreshing and the method update to refresh as we want. the module 'time' to perform the movement of the snake on the screen

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,10 +1,11 @@
 from turtle import Turtle, Screen
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
-
+screen.tracer(0)
 #CREATE SNAKE BODY
 #create an empty list of turtles
 turtles = []
@@ -18,8 +19,17 @@ for _ in range(3):
 #so that the result looks like a single object
 #I am using their index to set their xcor.
 for turtle in turtles:
+    turtle.penup()
     turtle.goto(turtles.index(turtle)*(-20), 0)
 
 #TODO 2 - MOVE THE SNAKE
+#create the variable game_is_on
+screen.update()
+game_is_on = True
+while game_is_on:
+    screen.update()
+    for square in turtles:
+        square.forward(20)
+        time.sleep(1)
 
 screen.exitonclick()
