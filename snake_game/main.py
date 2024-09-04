9fbf2a9024614621d092eb6dfa200e70909ed153
Bret Walda <82909109+Ja-Code-del@users.noreply.github.com>
2024-09-04 17:25:41 +0000

    USED SLICING IN COLLISION WITH TAIL DETECTION

--- snake_game/main.py
+++ snake_game/main.py
@@ -38,10 +38,8 @@ while game_is_on:
         scoreboard.game_over()
 
     #Dectect collision with the tail
-    for segment in snake.turtles:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    for segment in snake.turtles[1:]:
+        if snake.head.distance(segment) < 10:
             game_is_on = False
             scoreboard.game_over()
 
