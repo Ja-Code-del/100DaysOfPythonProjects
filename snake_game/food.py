a4c61c23abbb73aa622ea8feefbd4d694ec72289
Bret Walda <82909109+Ja-Code-del@users.noreply.github.com>
2024-09-04 23:21:50 +0000

    added a big food after each 5 refresh.

--- snake_game/food.py
+++ snake_game/food.py
@@ -2,26 +2,26 @@ from turtle import Turtle
 import random as r
 
 COLORS = [
-    "LightPink",     # Rose pastel clair
-    "LightSalmon",   # Saumon clair
-    "LightYellow",   # Jaune clair
-    "LightGreen",    # Vert clair
-    "LightBlue",     # Bleu clair
-    "Lavender",      # Lavande
-    "Pink",          # Rose
-    "PeachPuff",     # Pêche clair
-    "SkyBlue",       # Bleu ciel
-    "Thistle",       # Chardon
-    "Honeydew",      # Miel
-    "MistyRose"      # Rose brumeux
+    "LightPink",  # Rose pastel clair
+    "LightSalmon",  # Saumon clair
+    "LightYellow",  # Jaune clair
+    "LightGreen",  # Vert clair
+    "LightBlue",  # Bleu clair
+    "Lavender",  # Lavande
+    "Pink",  # Rose
+    "PeachPuff",  # Pêche clair
+    "SkyBlue",  # Bleu ciel
+    "Thistle",  # Chardon
+    "Honeydew",  # Miel
+    "MistyRose"  # Rose brumeux
 ]
-
 
 
 class Food(Turtle):
     def __init__(self):
         super().__init__()
         self.food_color = r.choice(COLORS)
+        self.apparition = 0
         self.shape("circle")
         self.penup()
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
@@ -31,8 +31,12 @@ class Food(Turtle):
         self.goto(self.x, self.y)
 
     def refresh(self):
+        self.apparition += 1
         self.food_color = r.choice(COLORS)
         self.color(self.food_color)
         self.x = r.randint(-280, 280)
         self.y = r.randint(-280, 280)
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        if self.apparition % 5 == 0:
+            self.shapesize(stretch_wid=1, stretch_len=1)
         self.goto(self.x, self.y)
